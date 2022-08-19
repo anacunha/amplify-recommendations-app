beed1c5fc88a0a112be53252581058b10592668d
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-08-19 20:54:59 -0300

    Import View from @aws-amplify/ui-react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Flex } from "@aws-amplify/ui-react";
+import { Flex, View } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
       Navigation bar
       <Flex overflow={'auto'}>
+        <View>
         List of recommendations
+        </View>
         <Flex position={'relative'} overflow={'hidden'} grow={1}>
         Details of recommendation
         </Flex>

c5c46187b1caf1230cf81ab14ed90cbc1043fe1b
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-08-19 20:27:59 -0300

    Initial app config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Navigation bar
+      <Flex overflow={'auto'}>
+        List of recommendations
+        <Flex position={'relative'} overflow={'hidden'} grow={1}>
+        Details of recommendation
+        </Flex>
+      </Flex>
     </div>
   );
 }

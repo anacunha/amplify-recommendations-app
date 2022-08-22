d4cfcef62fbaa0ff589ea86ea4d8f339d67647d5
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-08-22 10:01:52 -0300

    Use UI Components on App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Flex, View } from "@aws-amplify/ui-react";
+import { Flex, useBreakpointValue, View } from "@aws-amplify/ui-react";
+import { Item, NavBar } from './ui-components';
 
 function App() {
+  const sizeVariant = useBreakpointValue({
+    small: 'small',
+    medium: 'default'
+  })
+
   return (
     <div className="App">
-      Navigation bar
+      <NavBar width='100vw' size={sizeVariant} />
       <Flex overflow={'auto'}>
         <View>
-          List of recommendations
+          <Item />
+          <Item />
+          <Item />
+          <Item />
         </View>
         <Flex position={'relative'} overflow={'hidden'} grow={1}>
           {/* Details of recommendation */}
